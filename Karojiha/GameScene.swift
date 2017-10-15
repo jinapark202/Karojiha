32140dea4a3d5a904cc4628ec1a1d747506abc81
kheen <kheen@macalester.edu>
2017-10-15 18:46:24 -0500

    Added .gitignore and background structure

--- Karojiha/GameScene.swift
+++ Karojiha/GameScene.swift
@@ -30,28 +30,64 @@ class GameScene: SKScene {
     let ledge = SKNode()
     let playerBody = SKPhysicsBody(circleOfRadius: 30)
 
-
+    //Add desired background images to this array of strings. Makes sure background images are in Assets.xcassets
+    let backgroundNames: [String] = ["bg0","bg3","bg2"]
+    var backgroundImages: [SKNode] = []
+    let backgroundHeight = CGFloat(3.0) //This is height of background in terms of # of screens
+    var currentBackground: CGFloat = 1.0
+    var previousBackground: CGFloat = 0.0
+    
     override func didMove(to view: SKView) {
         createScene()
         createLedge()
         createPlayerAndPosition()
-
+        createClickLabel()
+        
+        initBackgroundArray(names: backgroundNames)
+        
         physicsWorld.gravity.dy = -15
         
         addChild(cameraNode)
         camera = cameraNode
         cameraNode.position = CGPoint(x: size.width/2, y: size.height/2)
         
-        clickLabel.horizontalAlignmentMode = .right
-        clickLabel.position = CGPoint(x: size.width/2.35, y: size.height/2.35)
-        clickLabel.fontColor = .white
-        clickLabel.fontSize = 15
-        clickLabel.fontName = "Avenir"
-        clickLabel.text = String("Clicks: ") + String(counter)
-        cameraNode.addChild(clickLabel)
+        addChild(backgroundImages[0])
 
+
+        
     }
     
+    
+    //This function creates SKSpriteNode Objects for all background images, and adds them to an array (backgroundImages)
+    func initBackgroundArray(names: [String]){
+        var x: CGFloat = 0.0
+        for i in names{
+            let backgroundImage = SKSpriteNode(imageNamed: i)
+            backgroundImage.xScale=size.width/backgroundImage.size.width
+            backgroundImage.yScale=size.height/backgroundImage.size.height*backgroundHeight
+            backgroundImage.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+            backgroundImage.position = CGPoint(x: size.width/2, y: backgroundImage.size.height*x)
+            backgroundImage.zPosition = -1
+            backgroundImages.append(backgroundImage)
+            x+=1
+            print(backgroundImage.position)
+        }
+        
+    }
+    
+    
+    //Set up click counter in upper right corner
+    func createClickLabel(){
+    
+    clickLabel.horizontalAlignmentMode = .right
+    clickLabel.position = CGPoint(x: size.width/2.35, y: size.height/2.35)
+    clickLabel.fontColor = .white
+    clickLabel.fontSize = 15
+    clickLabel.fontName = "Avenir"
+    clickLabel.text = String("Clicks: ") + String(counter)
+    cameraNode.addChild(clickLabel)
+    
+    }
     
     func createLedge() {
         ledge.position = CGPoint(x: size.width/2, y: size.height/40)
@@ -71,27 +107,29 @@ class GameScene: SKScene {
     }
     
     
-    func addObstacle() {
-        addSquareObstacle()
-    }
+    //TESTING OBSTACLE CODE
     
-    func addSquareObstacle() {
-        let path = UIBezierPath(roundedRect: CGRect.init(x: -200, y: -200,
-                                                         width: 400, height: 40),
-                                cornerRadius: 20)
-        
-        let obstacle = obstacleByDuplicatingPath(path, clockwise: false)
-        obstacles.append(obstacle)
-        obstacle.position = CGPoint(x: size.width/2, y: obstacleSpacing * CGFloat(obstacles.count))
-        addChild(obstacle)
-    }
-    
-    func obstacleByDuplicatingPath(_ path: UIBezierPath, clockwise: Bool) -> SKNode {
-        let container = SKNode()
-        let section = SKShapeNode(path: path.cgPath)
-        container.addChild(section)
-        return container
-    }
+//    func addObstacle() {
+//        addSquareObstacle()
+//    }
+//
+//    func addSquareObstacle() {
+//        let path = UIBezierPath(roundedRect: CGRect.init(x: -200, y: -200,
+//                                                         width: 400, height: 40),
+//                                cornerRadius: 20)
+//
+//        let obstacle = obstacleByDuplicatingPath(path, clockwise: false)
+//        obstacles.append(obstacle)
+//        obstacle.position = CGPoint(x: size.width/2, y: obstacleSpacing * CGFloat(obstacles.count))
+//        addChild(obstacle)
+//    }
+//
+//    func obstacleByDuplicatingPath(_ path: UIBezierPath, clockwise: Bool) -> SKNode {
+//        let container = SKNode()
+//        let section = SKShapeNode(path: path.cgPath)
+//        container.addChild(section)
+//        return container
+//    }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -120,15 +158,16 @@ class GameScene: SKScene {
     
     func dieAndRestart() {
         bird.physicsBody?.velocity.dy = 0
-        
-        for node in obstacles {
-            node.removeFromParent()
-        }
-        
-        obstacles.removeAll()
-        
-        addObstacle()
-        cameraNode.position = CGPoint(x: size.width/2, y: size.height/2)
+ 
+        // TESTING OBSTACLE CODE
+//        for node in obstacles {
+//            node.removeFromParent()
+//        }
+//
+//        obstacles.removeAll()
+//
+//        addObstacle()
+//        cameraNode.position = CGPoint(x: size.width/2, y: size.height/2)
         
         let scene = GameScene(size: size)
         view?.presentScene(scene)
@@ -139,11 +178,27 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
+        
+        
         clickLabel.text = String("Clicks: ") + String(counter)
 
-        if bird.position.y > obstacleSpacing * CGFloat(obstacles.count - 2) {
-            addObstacle()
+        //Adds the next background when the bird is close enough
+        if (bird.position.y > backgroundHeight*size.height*currentBackground - size.height){
+            addChild(backgroundImages[Int(currentBackground)])
+            currentBackground+=1
         }
+        
+        //Removes the previous background when the bird is far enough above it
+        if (bird.position.y > backgroundHeight*size.height*(previousBackground+1) + size.height){
+            (backgroundImages[Int(previousBackground)]).removeFromParent()
+            previousBackground+=1
+            
+        }
+        
+        // TESTING OBSTACLE CODE
+//        if bird.position.y > obstacleSpacing * CGFloat(obstacles.count - 2) {
+//            addObstacle()
+//        }
         
         let playerPositionInCamera = cameraNode.convert(bird.position, from: self)
         if playerPositionInCamera.y > 0 {
