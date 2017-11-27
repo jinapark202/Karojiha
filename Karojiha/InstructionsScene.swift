b54e85bb8ee29a0c87c0cd9bcb7f3c77f82e9f81
Hgray <hgray@macalester.edu>
2017-11-27 11:48:30 -0600

    created instructionsScene and added text

--- Karojiha/InstructionsScene.swift
+++ Karojiha/InstructionsScene.swift
@@ -8,27 +8,36 @@
 
 import Foundation
 import SpriteKit
+import UIKit
 
 class InstructionsScene: SKScene, SKPhysicsContactDelegate{
     override init(size: CGSize){
-
+        let titleLabel = SKLabelNode(fontNamed: "AvenirNextCondensed-DemiBold")
+        let stepsLabel = SKLabelNode(fontNamed: "Avenir-Light")
+        
         super.init(size: size)
         
-        let message = "How to Play"
-        instructionsLabel.text = message
-        instructionsLabel.fontSize = 30
-        instructionsLabel.fontColor = SKColor.white
-        instructionsLabel.position = CGPoint(x: size.width/2, y: size.height/2)
-        addChild(instructionsLabel)
-        instructionsLabel.run(SKAction.scale(to: 1.0, duration: 0.0))
+        let title = "How to Play"
+        titleLabel.text = title
+        titleLabel.fontSize = 60
+        titleLabel.fontColor = SKColor.white
+        titleLabel.position = CGPoint(x: size.width/2, y: size.height/1.5)
+        addChild(titleLabel)
+        titleLabel.run(SKAction.scale(to: 1.0, duration: 0.0))
+
+        let step1 = "Tap anywhere on the screen to make the bird flap it’s wings and move up the screen."
+        stepsLabel.text = step1
+        stepsLabel.fontSize = 25
+        stepsLabel.fontColor = SKColor.white
+        stepsLabel.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(stepsLabel)
+        stepsLabel.run(SKAction.scale(to: 1.0, duration: 0.0))
         
-        let message1 = "Tap the screen to make the bird fly up. Then "
-        instructionsLabel.text = message1
-        instructionsLabel.fontSize = 30
-        instructionsLabel.fontColor = SKColor.white
-        instructionsLabel.position = CGPoint(x: size.width/2, y: size.height/2.2)
-        addChild(instructionsLabel)
-        instructionsLabel.run(SKAction.scale(to: 1.0, duration: 0.0))
+        //for the future...
+//        let step2 = "Catch dragonflies to gain power ups and keep going for longer"
+//        let step3 = "But be careful—gravity is trying to pull you down the more times you flap your       wings."
+//        let step4 = "Try to fly as high as you can into space. You are an explorer into outer space. It’s the last new frontier!"
+        
     }
     
     required init(coder aDecoder: NSCoder) {
